6374cbd1578cbfec321f002f5b132a193999ef83
Arjan Tijms <arjan.tijms@omnifish.ee>
2023-12-04 14:43:11 +0100

    Processing feedback. Make Credentials inner type
    Signed-off-by: Arjan Tijms <arjan.tijms@omnifish.ee>

--- api/src/main/java/jakarta/security/enterprise/identitystore/InMemoryIdentityStoreDefinition.java
+++ api/src/main/java/jakarta/security/enterprise/identitystore/InMemoryIdentityStoreDefinition.java
@@ -19,6 +19,9 @@ package jakarta.security.enterprise.identitystore;
 
 import static jakarta.security.enterprise.identitystore.IdentityStore.ValidationType.PROVIDE_GROUPS;
 import static jakarta.security.enterprise.identitystore.IdentityStore.ValidationType.VALIDATE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -47,6 +50,8 @@ import java.lang.annotation.Target;
  * </code>
  * </pre>
  *
+ * <b>NOTE: DRAFT API. MAY CHANGE IN THE FINAL VERSION</b>
+ *
  */
 @Retention(RUNTIME)
 @Target(TYPE)
@@ -71,5 +76,36 @@ public @interface InMemoryIdentityStoreDefinition {
      * @return the type the identity store is used for
      */
     ValidationType[] useFor() default {VALIDATE, PROVIDE_GROUPS};
+
+    /**
+     * <code>Credentials</code> define a single caller identity for
+     * use with the {@link InMemoryIdentityStoreDefinition} annotation.
+     *
+     */
+    @Retention(RUNTIME)
+    @Target({ TYPE, METHOD, FIELD, PARAMETER })
+    public @interface Credentials {
+
+        /**
+         * Name of caller. This is the name a caller uses to authenticate with.
+         *
+         * @return Name of caller
+         */
+        String callerName();
+
+        /**
+         * A text-based password used by the caller to authenticate.
+         *
+         * @return A text-based password
+         */
+        String password();
+
+        /**
+         * The optional list of groups that the specified caller is in.
+         *
+         * @return optional list of groups
+         */
+        String[] groups() default {};
+    }
 
 }
