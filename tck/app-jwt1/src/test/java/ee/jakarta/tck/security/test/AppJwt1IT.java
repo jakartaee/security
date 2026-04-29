2be49250e0adc9ff297edae987670d4208ade32e
Arjan Tijms <arjan.tijms@omnifish.ee>
2026-04-30 00:19:42 +0200

    Start 5.0 with Initial JWT support
    * Set version to 5.0.0-SNAPSOT
    * Add draft annotation for JWT Authentication mechanism
    * Add initial TCK test for a minimal JWT authentication

---
+++ tck/app-jwt1/src/test/java/ee/jakarta/tck/security/test/AppJwt1IT.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation.
+ * Copyright (c) 2015, 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package ee.jakarta.tck.security.test;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+import org.htmlunit.DefaultCredentialsProvider;
+import org.htmlunit.WebResponse;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static ee.jakarta.tck.security.test.Assert.assertDefaultAuthenticated;
+import static ee.jakarta.tck.security.test.Assert.assertDefaultNotAuthenticated;
+import static ee.jakarta.tck.security.test.ShrinkWrap.mavenWar;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(Arquillian.class)
+public class AppJwt1IT extends ArquillianBase {
+
+    @Deployment(testable = false)
+    public static Archive<?> createDeployment() {
+        return mavenWar();
+    }
+
+    @Test
+    public void testAuthenticated() {
+    	DefaultCredentialsProvider credentialsProvider = new DefaultCredentialsProvider();
+    	credentialsProvider.addCredentials("reza", "secret1".toCharArray());
+
+    	getWebClient().addRequestHeader("Authorization", "Bearer " + getJWE());
+
+        assertDefaultAuthenticated(
+            readFromServer("/servlet"));
+    }
+
+    @Test
+    public void testNotAuthenticated() {
+        WebResponse response = responseFromServer("/servlet");
+
+        assertEquals(401, response.getStatusCode());
+
+        assertTrue(
+            "Response did not contain the \"WWW-Authenticate\" header, but should have",
+            response.getResponseHeaderValue("WWW-Authenticate") != null);
+
+        assertDefaultNotAuthenticated(
+            response.getContentAsString());
+    }
+//
+//    @Test
+//    public void testNotAuthenticatedWrongName() {
+//    	DefaultCredentialsProvider credentialsProvider = new DefaultCredentialsProvider();
+//    	credentialsProvider.addCredentials("romo", "secret1".toCharArray());
+//
+//    	getWebClient().setCredentialsProvider(credentialsProvider);
+//
+//    	WebResponse response = responseFromServer("/servlet");
+//
+//    	assertEquals(401, response.getStatusCode());
+//
+//    	assertTrue(
+//	        "Response did not contain the \"WWW-Authenticate\" header, but should have",
+//	        response.getResponseHeaderValue("WWW-Authenticate") != null);
+//
+//    	assertDefaultNotAuthenticated(
+//	        response.getContentAsString());
+//    }
+//
+//    @Test
+//    public void testNotAuthenticatedWrongPassword() {
+//
+//      	DefaultCredentialsProvider credentialsProvider = new DefaultCredentialsProvider();
+//    	credentialsProvider.addCredentials("reza", "wrongpassword".toCharArray());
+//
+//    	getWebClient().setCredentialsProvider(credentialsProvider);
+//
+//        WebResponse response = responseFromServer("/servlet");
+//
+//        assertEquals(401, response.getStatusCode());
+//
+//        assertTrue(
+//            "Response did not contain the \"WWW-Authenticate\" header, but should have",
+//            response.getResponseHeaderValue("WWW-Authenticate") != null);
+//
+//        assertDefaultNotAuthenticated(
+//            response.getContentAsString());
+//    }
+
+    private String getJWE() {
+        try {
+            return new String(getClass().getClassLoader().getResourceAsStream("token.jws").readAllBytes(), UTF_8);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+}
